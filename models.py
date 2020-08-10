d1d6d4e626ad27cc6f8b07be9de0cd26c5a1240a
Sergey Andreev <sergey666andreev@gmail.com>
2020-08-10 18:15:33 +0600

    feat: Add model implementation
    Implement model for facial keypoints detection using PyTorch

--- models.py
+++ models.py
@@ -21,6 +21,18 @@ class Net(nn.Module):
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv5 = nn.Conv2d(256, 512, 1)
+
+        self.maxPooling = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(6 * 6 * 512, 1024)
+        self.fc2 = nn.Linear(1024, 1024)
+        self.fc3 = nn.Linear(1024, 136)
+
+        self.dropout = nn.Dropout(0.25)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
@@ -32,6 +44,17 @@ class Net(nn.Module):
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
         
-        
+        x = self.maxPooling(F.elu(self.conv1(x)))
+        x = self.maxPooling(F.elu(self.conv2(x)))
+        x = self.maxPooling(F.elu(self.conv3(x)))
+        x = self.maxPooling(F.elu(self.conv4(x)))
+        x = self.maxPooling(F.elu(self.conv5(x)))
+
+        x = x.view(x.size(0), -1)
+
+        x = self.dropout(F.elu(self.fc1(x)))
+        x = self.dropout(F.elu(self.fc2(x)))
+        x = self.fc3(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
