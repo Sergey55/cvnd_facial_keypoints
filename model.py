eaa09f847535e2ef06bb18678a0e980ad3f2ea6b
Sergey Andreev <sergey666andreev@gmail.com>
2020-10-30 10:50:11 +0600

    chore: models.py -> model.py

--- model.py
+++ model.py
@@ -1,25 +1,23 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
+import torch.optim as optim
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
+import pytorch_lightning as pl
 
-class Net(nn.Module):
+
+class Net(pl.LightningModule):
+    """Neural ntwork for detecting facial keypoints"""
 
     def __init__(self):
+        """Constructor"""
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
-        
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
         self.conv1 = nn.Conv2d(1, 32, 5)
         self.conv2 = nn.Conv2d(32, 64, 3)
         self.conv3 = nn.Conv2d(64, 128, 3)
@@ -33,17 +31,11 @@ class Net(nn.Module):
         self.fc3 = nn.Linear(1024, 136)
 
         self.dropout = nn.Dropout(0.25)
-        
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
 
+        self.criterion = nn.MSELoss()
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
+
         x = self.maxPooling(F.elu(self.conv1(x)))
         x = self.maxPooling(F.elu(self.conv2(x)))
         x = self.maxPooling(F.elu(self.conv3(x)))
@@ -56,5 +48,19 @@ class Net(nn.Module):
         x = self.dropout(F.elu(self.fc2(x)))
         x = self.fc3(x)
 
-        # a modified x, having gone through all the layers of your model, should be returned
         return x
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=0.001)
+
+        return optimizer
+
+    def training_step(self, batch, batch_idx):
+        images, key_points = batch
+        images, key_points = images.type(torch.FloatTensor), key_points.type(torch.FloatTensor)
+
+        y_hat = self(images)
+
+        loss = self.criterion(y_hat, key_points)
+
+        return {'loss': loss}
