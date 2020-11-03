e3a61abe00179b63442a2cd9e354fb5c8a01f7c8
Sergey Andreev <sergey666andreev@gmail.com>
2020-11-03 10:50:00 +0600

    feat: Use `relu` instead of `elu`

--- model.py
+++ model.py
@@ -37,21 +37,21 @@ class Net(pl.LightningModule):
         
     def forward(self, x):
 
-        x = self.maxPooling(F.elu(self.conv1(x)))
+        x = self.maxPooling(F.relu(self.conv1(x)))
         x = self.dropout1(x)
-        x = self.maxPooling(F.elu(self.conv2(x)))
+        x = self.maxPooling(F.relu(self.conv2(x)))
         x = self.dropout1(x)
-        x = self.maxPooling(F.elu(self.conv3(x)))
+        x = self.maxPooling(F.relu(self.conv3(x)))
         x = self.dropout1(x)
-        x = self.maxPooling(F.elu(self.conv4(x)))
+        x = self.maxPooling(F.relu(self.conv4(x)))
         x = self.dropout1(x)
-        x = self.maxPooling(F.elu(self.conv5(x)))
+        x = self.maxPooling(F.relu(self.conv5(x)))
         x = self.dropout1(x)
 
         x = x.view(x.size(0), -1)
 
-        x = self.dropout2(F.elu(self.fc1(x)))
-        x = self.dropout2(F.elu(self.fc2(x)))
+        x = self.dropout2(F.relu(self.fc1(x)))
+        x = self.dropout2(F.relu(self.fc2(x)))
         x = self.fc3(x)
 
         return x
