5571d0887518c336f252d80d3772b550c6480b01
Sergey Andreev <sergey666andreev@gmail.com>
2020-11-03 09:25:41 +0600

    feat: Change model architecture

--- model.py
+++ model.py
@@ -19,33 +19,39 @@ class Net(pl.LightningModule):
         super(Net, self).__init__()
         
         self.conv1 = nn.Conv2d(1, 32, 5)
-        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv2 = nn.Conv2d(32, 64, 4)
         self.conv3 = nn.Conv2d(64, 128, 3)
         self.conv4 = nn.Conv2d(128, 256, 3)
-        self.conv5 = nn.Conv2d(256, 512, 1)
+        self.conv5 = nn.Conv2d(256, 512, 3)
 
         self.maxPooling = nn.MaxPool2d(2, 2)
 
-        self.fc1 = nn.Linear(6 * 6 * 512, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
+        self.fc1 = nn.Linear(4 * 4 * 512, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
         self.fc3 = nn.Linear(1024, 136)
 
-        self.dropout = nn.Dropout(0.25)
+        self.dropout1 = nn.Dropout(0.1)
+        self.dropout2 = nn.Dropout(0.25)
 
         self.criterion = nn.MSELoss()
         
     def forward(self, x):
 
         x = self.maxPooling(F.elu(self.conv1(x)))
+        x = self.dropout1(x)
         x = self.maxPooling(F.elu(self.conv2(x)))
+        x = self.dropout1(x)
         x = self.maxPooling(F.elu(self.conv3(x)))
+        x = self.dropout1(x)
         x = self.maxPooling(F.elu(self.conv4(x)))
+        x = self.dropout1(x)
         x = self.maxPooling(F.elu(self.conv5(x)))
+        x = self.dropout1(x)
 
         x = x.view(x.size(0), -1)
 
-        x = self.dropout(F.elu(self.fc1(x)))
-        x = self.dropout(F.elu(self.fc2(x)))
+        x = self.dropout2(F.elu(self.fc1(x)))
+        x = self.dropout2(F.elu(self.fc2(x)))
         x = self.fc3(x)
 
         return x
